e685da5d447bd9280ee5b81d6f1c67b7eda928f6
RPGP1 <great.super.science@gmail.com>
2014-12-13 14:56:22 +0900

    File入出力関係の整備

--- lib/rpg.rb
+++ lib/rpg.rb
@@ -7,6 +7,12 @@ module RPG
   include DXRuby
 end
 
+Dir.chdir(File.dirname(__FILE__)) do
+  Dir.mkdir('../data') unless Dir.exist?('../data')
+  Dir.mkdir('../data/tileset') unless Dir.exist?('../data/tileset')
+  Dir.mkdir('../data/map') unless Dir.exist?('../data/map')
+end
+
 require_relative './common/exception'
 require_relative './common/image_marshal'
 require_relative './map/map'
